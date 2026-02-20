02dffd45cf4d6dd42ff5452dc3ff1e6f2e71f24e
Fredrik <fredrik.midtfla@gmail.com>
2026-02-20 14:07:30 +0100

    Added some repo tests in program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using uke1.Classes;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Repository<int> repositoryInt = new Repository<int>();
+        Repository<string> repositoryString = new Repository<string>();
+        Repository<double> repositoryDouble = new Repository<double>();
+
+        repositoryDouble.Add(7.5);
+        repositoryInt.Add(7);
+
+        var doubleTimesInt = repositoryDouble.GetByIndex(0) * repositoryInt.GetByIndex(0);
+        Console.WriteLine($"repDouble * repInt = {doubleTimesInt}");
+    }
+}
